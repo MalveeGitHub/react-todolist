f4f59d4d3139582fd9018330e94253d9377d6851
alvee <you@example.com>
2018-10-20 06:33:07 -0400

    Updates

---
+++ precache-manifest.f0d71a72dc7a3a849d97e5fb6c24e73a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dcf16d97d4f84f0f9300",
+    "url": "/static/js/main.dcf16d97.chunk.js"
+  },
+  {
+    "revision": "02e78e898367c9391eca",
+    "url": "/static/js/1.02e78e89.chunk.js"
+  },
+  {
+    "revision": "dcf16d97d4f84f0f9300",
+    "url": "/static/css/main.022c5e4d.chunk.css"
+  },
+  {
+    "revision": "fec234853a558a43e6722c492e2baa33",
+    "url": "/index.html"
+  }
+];
